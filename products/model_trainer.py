14259a5f686c0b9f93ddc6b269c16477b93848ee
Carlo C del Mundo <carlodelmundo@users.noreply.github.com>
2018-02-04 17:41:44 -0800

    Model training only available for devices with CUDA GPUs. (#16)
    In support of #10.
    
    * For model training, only allow devices with GPUs to train.
    * Add a new command line parameter in `model_trainer.py`: `dataset` and `model_path`. `dataset` is a required field that specifies a dataset to train on. `model_path`, if selected, loads the model at a specific path. If the `model_path` is not selected, defaults on the pretrained ResNet-18 model.
    * Models are saved every 1000 epochs in `~/regression-model.pkl`.
    * Training is indefinite until the process is explicitly killed.

--- products/model_trainer.py
+++ products/model_trainer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 # Author: Carlo C. del Mundo <cdel@cs.washington.edu>
+# Trains a regression model given a regression dataset. Trained model snapshots
+# are saved in _MODEL_SNAPSHOT_DIR.
+# Usage:
+#  bazel run :model_trainer -- --dataset /opt/datasets/sample_dataset
+# Sample output:
+#  Mean squared error (loss) = 0.7843878865242004
+import argparse
 from core import regression_dataset
+import os
+import sys
 import torch
 from torch import nn
 from torch import optim
@@ -8,8 +17,11 @@ from torchvision.models import resnet
 from torchvision import transforms
 from torch.autograd import Variable
 
-_SAMPLE_DATASET_PATH = "/opt/datasets/sample_dataset/"
-_MODEL_PATH = "external/resnet18/file/regression-model.pkl"
+# Which base model to use. This model is a pretrained ResNet-18 model trained
+# for classification, but repurposed for regression.
+_DEFAULT_MODEL_PATH = "external/resnet18/file/regression-model.pkl"
+# Save models in the user's home directory. Only the latest model is saved.
+_MODEL_SNAPSHOT_DIR = "~/"
 
 
 def _loader(dataset_path, batch_size):
@@ -29,25 +41,54 @@ def _loader(dataset_path, batch_size):
     return data_loader
 
 
-def train_network(batch_size, epochs):
-    train_loader = _loader(_SAMPLE_DATASET_PATH, batch_size)
+def _save_model(model):
+    output_dir = os.path.expanduser(_MODEL_SNAPSHOT_DIR)
+    output_path = os.path.join(output_dir, "regression-model.pkl")
+    print("Snapshotting model to {}".format(output_path))
+    torch.save(model.state_dict(), output_path)
+
+
+def train_network(model_path, dataset, batch_size):
+    train_loader = _loader(dataset, batch_size)
     # Typically, num_classes specifies the number of object classes in image
     # classification. Here, we fix num_classes to 1 since we are regressing,
     # and not classifying. The output of an inference is a single number.
     net = resnet.resnet18(num_classes=1)
-    net.load_state_dict(torch.load(_MODEL_PATH))
+    net.load_state_dict(torch.load(model_path))
+    net.cuda()
     criterion = nn.MSELoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    for epoch in range(epochs):
+    # Train indefinitely.
+    for epoch in range(sys.maxsize):
         for image_data, scores in train_loader:
             optimizer.zero_grad()
-            inputs, labels = Variable(image_data), Variable(scores.float())
+            inputs, labels = Variable(image_data.cuda()), Variable(
+                scores.float().cuda())
             outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
             print("Mean squared error (loss) = {}".format(loss.data[0]))
+        if (epoch + 1) % 1000 == 0:
+            _save_model(net)
 
 
 if __name__ == "__main__":
-    train_network(batch_size=4, epochs=512)
+    parser = argparse.ArgumentParser(description="Trains a model.")
+    parser.add_argument(
+        "--dataset",
+        required=True,
+        help="The directory containing a training dataset. A dataset consists"
+        " of images and a dataset.json file")
+    parser.add_argument(
+        "--model_path",
+        required=False,
+        default=_DEFAULT_MODEL_PATH,
+        help="The path to the model weights (.pkl) file.")
+    args = parser.parse_args()
+    if not torch.cuda.is_available():
+        raise "Training is only supported on devices with CUDA GPUs."
+    train_network(
+        os.path.expanduser(args.model_path),
+        os.path.expanduser(args.dataset),
+        batch_size=64)
